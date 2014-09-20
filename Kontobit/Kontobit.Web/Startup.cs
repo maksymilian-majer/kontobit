856d8f156ca2660582dd21f85cc62fb4ebe726b7
Maksymilian Majer <maksymilian.majer@gmail.com>
2014-09-20 13:36:17 +0200

    * initial solution for Kontobit with Google+ authentication

---
+++ Kontobit/Kontobit.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Kontobit.Web.Startup))]
+namespace Kontobit.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
